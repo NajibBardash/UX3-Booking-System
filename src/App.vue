71243bae93db9ed87a4d05484c09a240c306708b
Najib Bardash <najib.bardash@gmail.com>
2025-05-06 11:36:19 +0200

    Initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TopBar from './components/TopBar.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <TopBar msg="Svenssons Hantverk AB" />
     </div>
+
   </header>
 
   <main>
-    <TheWelcome />
+
+    <div>
+      <p>Some text</p>
+    </div>
+
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    background-color: rgba(139, 157, 134, 1);
+    border: 1px solid red;
+    width: 100%;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  main {
+    background-color: white;
+    border: 1px solid red;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="topBar">
+    <h1 class="main-header">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+  .topBar {
+    background-color: rgba(139, 157, 134, 1);
+
+  }
+</style>
